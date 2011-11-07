8bb9ec1d5c863ced3384cfa7ec0da9369c3a842e
Frederik B <fbraun@mozilla.com>
2011-11-07 13:22:54 -0800

    change naming to avoid confusion

--- Garmr/scanner.py
+++ Garmr/scanner.py
@@ -24,34 +24,34 @@ def get_url(url, status = True):
 class PassiveTest():
     secure_only = False
     insecure_only = False
-    
+
     def analyze(self, response, results):
         return None
-    
+
     def result(self, state, message, data):
         return {'state' : state,  'message' : message, 'data' : data }
-        
 
-class ActiveTest(): 
-    
+
+class ActiveTest():
+
     secure_only = False
     insecure_only = False
     run_passives = True
     description = "The base class for an Active Test."
-    
+
     def __init__(self):
         if hasattr(self, "setup"):
             self.setup()
-    
+
     def execute(self, url):
-        try:            
-            result = self.do_test(url)
+        try:
+            resulttyple = self.do_test(url)
         except Exception, e:
             tb = traceback.format_exc()
-            result = (ActiveTest().result("Error", e, tb), None)
-        
-        return result
-             
+            resulttuple = (ActiveTest().result("Error", e, tb), None)
+
+        return resulttuple
+
     def result(self, state, message, data):
         return { 'state' : state, 'message' : message, 'data' : data, 'passive' : {}}
 
@@ -64,16 +64,16 @@ class HtmlTest(PassiveTest):
         else:
             result = self.result("Skip", "Content-type is not html "+ response.headers['content-type'], None)
             return result
-            
+
     def analyze_html(self, response, soup):
         """ implement this method in subclass"""
-        pass 
-    
+        pass
+
 class Scanner():
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s')
     logger = logging.getLogger("Garmr-Scanner")
     logger.setLevel(logging.DEBUG)
-    
+
     def __init__(self):
         self.resolve_target = True
         self.force_passives = False
@@ -84,10 +84,10 @@ class Scanner():
         Scanner.logger.debug("Scanner initialized.")
         self.reporter = Reporter()
         self.modules = []
-    
+
     def do_passive_scan(self, passive, is_ssl, response):
         if passive.secure_only and not is_ssl:
-            Scanner.logger.debug("\t\t[%s] Skip Test invalid for http scheme" % passive.__class__)                        
+            Scanner.logger.debug("\t\t[%s] Skip Test invalid for http scheme" % passive.__class__)
             passive_result = PassiveTest().result("Skip", "This check is only applicable to SSL requests.", None)
             start = datetime.now()
             passive_result['start'] = start
@@ -103,7 +103,7 @@ class Scanner():
             passive_result['duration'] = float((td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6)) / 10**6
             Scanner.logger.info("\t\t[%s] %s %s" % (passive.__class__, passive_result['state'], passive_result['message']))
         return passive_result
-    
+
     def do_active_scan(self, test, is_ssl, target):
         if (test.secure_only and not is_ssl):
             Scanner.logger.info("\t[Skip] [%s] (reason: secure_only)" % test.__class__)
@@ -112,7 +112,7 @@ class Scanner():
             result['end'] = result['start']
             result['duration'] = 0
             return result
-        elif (test.insecure_only and is_ssl):
+        elif (test.insecure_only and is_ssl):ygjb
             Scanner.logger.info("\t[Skip] [%s] (reason: insecure_only)" % test.__class__)
             result = ActiveTest().result("Skip", "This check is only applicable to SSL requests", None)
             result['start'] = datetime.now()
@@ -134,12 +134,13 @@ class Scanner():
             result['passive'] = {}
             self.reporter.start_passives()
             for passive_key in self._passive_tests_.keys():
-                passive = self._passive_tests_[passive_key]["test"]                    
+                passive = self._passive_tests_[passive_key]["test"]
                 result["passive"][passive.__class__] = self.do_passive_scan(passive, is_ssl, response)
                 self.reporter.write_passive(passive.__class__, result["passive"][passive.__class__])
             self.reporter.end_passives()
+
         return result
-    
+
     def scan_target(self, target):
         self.reporter.write_target(target)
         Scanner.logger.info("[%s] scanning:" % target)
@@ -152,7 +153,7 @@ class Scanner():
             results[test.__class__] = self.do_active_scan(test, is_ssl, target)
         self.reporter.end_actives()
         return results
-    
+
     def run_scan(self):
         results = {}
         self.reporter.start_report()
@@ -170,12 +171,12 @@ class Scanner():
         file.write(self.reporter.end_report())
         file.close()
 
-    
+
     def register_target(self, url):
         u = urlparse(url)
         valid = u.netloc != "" and u.scheme in self._protos_
         reason = "%s%s" % ("[bad netloc]" if u.netloc == "" else "", "" if u.scheme in self._protos_ else "[bad scheme]")
-        
+
         # todo - support ipv6 urls
         host = u.netloc.split(':')[0]
         if (self.resolve_target):
@@ -191,7 +192,7 @@ class Scanner():
             Scanner.logger.debug("[target]: %s" % url)
             return
         Scanner.logger.error("%s is not a valid target (reason: %s)" % (url, reason))
-        
+
     def configure_check(self, check_name, key, value):
         if self._active_tests_.has_key(check_name):
             check = self._active_tests_[check_name]["test"]
@@ -205,7 +206,7 @@ class Scanner():
             raise Exception("%s is not a valid configuration for %s", key, check_name)
         check.config[key] = value
         Scanner.logger.debug("\t%s.%s=%s" % (check_name, key, value))
-    
+
     def disable_check(self, check_name):
         if self._active_tests_.has_key(check_name):
             self._active_tests_[check_name]["enabled"] = False
@@ -214,13 +215,13 @@ class Scanner():
         else:
             raise Exception("The requested check is not available (%s)" % check_name)
         Scanner.logger.debug("\t%s disabled.", check_name)
-        
+
     def register_check(self, test):
         module = test.__class__.__module__
-        
+
         if module not in self.modules:
             self.modules.append(module)
-            
+
         key = "%s" % test.__class__
         if isinstance(test, ActiveTest):
             self._active_tests_[key]= { "test" : test , "enabled" : True}
@@ -231,7 +232,7 @@ class Scanner():
             Scanner.logger.debug("Added %s to passive tests." % test.__class__)
             return len(self._passive_tests_)
         raise Exception('test is not a valid test type')
-    
+
     def save_configuration(self, path):
         # write out a configuration file.
         config = ConfigParser.RawConfigParser()
@@ -241,20 +242,20 @@ class Scanner():
         config.set("Garmr", "reporter", self.reporter.__class__)
         config.set("Garmr", "output", self.output)
         config.set("Garmr", "dns", self.resolve_target)
-        
+
         if len(self._targets_) > 0:
             config.add_section("Targets")
             i = 0
             for target in self._targets_:
                 config.set("Targets", "%s"%i, target)
-        
+
         for check in self._active_tests_.keys():
             config.add_section(check)
             config.set(check, "enabled", self._active_tests_[check]["enabled"])
             if hasattr(self._active_tests_[check]["test"], "config"):
                 for key in self._active_tests_[check]["test"].config.keys():
                     config.set(check, key, self._active_tests_[check]["test"].config[key])
-        
+
         for check in self._passive_tests_.keys():
             config.add_section(check)
             config.set(check, "enabled", self._passive_tests_[check]["enabled"])
@@ -262,8 +263,8 @@ class Scanner():
                 for key in self._passive_tests_[check]["test"].config.keys():
                     config.set(check, key, self._passive_tests_[check]["test"].config[key])
 
-                    
+
         with open(path, 'w') as configfile:
             config.write(configfile)
-        
-        
+
+
