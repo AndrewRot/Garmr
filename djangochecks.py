606b1aebc99c7d5988f074ee8ff29613141e0381
Frederik B <fbraun@mozilla.com>
2011-11-21 14:31:00 -0800

    two django checks added

--- djangochecks.py
+++ djangochecks.py
@@ -1,6 +1,6 @@
 from urlparse import urlparse
 import requests
-from Garmr.scanner import ActiveTest, PassiveTest, Scanner
+from Garmr.scanner import ActiveTest, PassiveTest, Scanner, HtmlTest
 
 
 class AdminAvailable(ActiveTest):
@@ -17,6 +17,28 @@ class AdminAvailable(ActiveTest):
         else:
             result = self.result("Fail", "Default Django admin page is not present at %s" % adminurl, None)
         return (result, response);
+
+
+class ProvokeError404(ActiveTest):
+    run_passives = True # we need IsDebugModeReallyEnabled
+    def do_test(self, url):
+        sess = self.sessions(url)
+        url += '76976cd1a3cbadaf77533a' #random garbage
+        response = sess.get(url)
+        result = self.result('Skip', 'This test cannot Pass or Fail, because it relies on the subsequent passive IsDebugModeReallyEnabled test', response)
+        return result, response
+
+class IsDebugModeReallyEnabled(HtmlTest):
+    description = ''
+    secure_only = False
+    def analyze_html(self, response, soup):
+        # we dont really analye the soup,  but that's ok;p
+        error_str = "You're seeing this error because you have" #from django source django/views/debug.py - maybe subject to change
+        if error_str in respnose.content:
+            result = self.result('Fail', 'Typical string of echnical 404/500 error page found', None)
+        else:
+            result = self.result('Pass', 'Debug strings not found', response)
+        return result
 
 
 def configure(scanner):
